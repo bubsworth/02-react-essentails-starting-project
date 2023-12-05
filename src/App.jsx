708b95ab226aaca4e5921b0f0d91b346008d0a54
bubsworth <bubbard96@hotmail.com>
2023-12-05 17:04:06 +0000

    [WIP] Add in header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-function App() {
+import Header from './components/Header';
+
+export default function App() {
   return (
-    <h1>React Investment Calculator</h1>
+    <Header />
   )
 }
-
-export default App
