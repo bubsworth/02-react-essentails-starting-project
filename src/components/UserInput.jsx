34bdeba61b03de1ea78d719ad0149dddafbe3cf3
bubsworth <bubbard96@hotmail.com>
2023-12-06 15:06:15 +0000

    Update user input styling

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,7 +1,18 @@
 export default function UserInput() {
     return (
         <div id="user-input">
-
+            <div className="input-group">
+                <label>Intial investment</label>
+                <input/>
+                <label>Annual Investment</label>
+                <input/>
+            </div>
+            <div className="input-group">
+                <label>Expected return</label>
+                <input/>
+                <label>Duration</label>
+                <input/>
+            </div>
         </div>
     );
 }
