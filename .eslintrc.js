46420699af401dfcdf7d4b661d0d61900f16c3ad
bubsworth <bubbard96@hotmail.com>
2023-12-07 15:44:14 +0000

    Add eslint/prettier

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react"],
+  rules: {
+    "react/jsx-uses-react": "off", // Disables the need to import React
+    "react/react-in-jsx-scope": "off", // Same as above
+  },
+};
